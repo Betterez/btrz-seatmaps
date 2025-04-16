7abdc861d53cc6f4d80d52b03ad3446126582413
DavidEGaleano <degnexo@gmail.com>
2025-04-16 16:34:38 -0300

    [DG] #86dwcrh4h fix a seat selection synch issue
     between backoffice and websales adding a especial case when the message comes from websales with a custom containerSelector that is incompatible with Backoffice leading to fail to find where to draw the blocked seat by the user in websales

--- src/seatmap-section.js
+++ src/seatmap-section.js
@@ -1496,7 +1496,14 @@ class SeatmapSection {
     const sectionSelector = elem.sectionId ? `[data-section="${elem.sectionId}"]` : ``;
     const containerSelector = elem.containerId ? `[data-container-id="${elem.containerId}"]` : ``
     const selector = `${containerSelector}${sectionSelector}[style*='grid-area: ${elem.row} / ${elem.col} / ${parseInt(elem.row, 10) + (elem.height || 1)} / ${parseInt(elem.col, 10) + (elem.width || 1)};']`;
+    const altSelector = `${sectionSelector}[style*='grid-area: ${elem.row} / ${elem.col} / ${parseInt(elem.row, 10) + (elem.height || 1)} / ${parseInt(elem.col, 10) + (elem.width || 1)};']`;
+    
     const element = document.querySelector(`[data-type="seat"]${selector}`);
+
+    if(!element){
+      element = document.querySelector("[data-type=\"seat\"]".concat(altSelector));
+    }
+
     if (element) {
       const newStatusText = `${status.charAt(0).toUpperCase()}${status.slice(1)}`;
       const oldStatusText = `${element.dataset.status.charAt(0).toUpperCase()}${element.dataset.status.slice(1)}`;
